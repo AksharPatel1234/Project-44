e126deef1aef92b4e64826ebe6108538d0e3ff38
AksharPatel1234 <102331801+AksharPatel1234@users.noreply.github.com>
2022-07-05 11:24:55 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,16 +2,22 @@ var p1Group,p2Group;
 var edges;
 var p1,p2;
 var bird;
-var GameState = 1
+var GameState = 1;
+var score = 0;
+touching = true;
 
 function setup(){
   createCanvas(500,500)
 
    p1Group = createGroup()
    p2Group = createGroup()
+   p3Group = createGroup()
 
   bird = createSprite(100,250,20,20);
- edges = createEdgeSprites()
+
+  edges = createEdgeSprites()
+  leftEdge = createEdgeSprites()
+  rightEdge = createEdgeSprites()
   
 }
 
@@ -23,6 +29,13 @@ function createPlatforms(){
     p2 = createSprite(500,random(100,0),20,random(200,300))
     p2.velocityX = -3
     p2Group.add(p2)
+
+    p3 = createSprite(500,random(100,0),1,random(500,1000))
+    p3.velocityX = -3
+    p3.visible = 0
+    p3Group.add(p3)
+
+
   }
   
 }
@@ -35,12 +48,27 @@ function createPlatforms(){
 
 function draw(){
   background(40)
+
+text("score:"+score,150,10)
+
   if(GameState == 1){
     createPlatforms();
+
+   if(bird.isTouching(p3Group) && touching){
+    touching = false;
+    score = score + 1/4
+ 
+    
+   }
+   else if(!touching){
+    touching = true;
+   }
 
     if(bird.collide(p1Group) || bird.collide(p2Group) || bird.collide(edges) ){
       GameState = 0;
       }
+     
+
   }
   if(GameState == 0){
     text("GAME OVER",250,250)
